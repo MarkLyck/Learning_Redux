79d948155f04eb789008b4635e77c0437cb7c9f8
Mark Lyck <mark.lyck@gmail.com>
2016-09-13 17:50:19 -0500

    Finished Redux tutorial

--- dev/js/components/App.js
+++ dev/js/components/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import UserList from '../containers/user-list';
-import UserDetails from '../containers/user-detail';
-require('../../scss/style.scss');
+import React from 'react'
+require('../../scss/style.scss')
+import UserList from '../containers/UserList'
+import UserDetail from '../containers/UserDetail'
 
 const App = () => (
-    <div>
-        <h2>User List</h2>
-        <UserList />
-        <hr />
-        <h2>User Details</h2>
-        <UserDetails />
-    </div>
-);
+  <div>
+    <h2>Username list:</h2>
+    <UserList/>
+    <hr/>
+    <h2>User Details:</h2>
+    <UserDetail/>
+  </div>
+)
 
-export default App;
+export default App
